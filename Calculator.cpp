6318185ce0c7a6cd1a1084816f0fe39c91abf9da
1998 <amjath.saidumuhammed@trenser.com>
2026-08-19 13:03:49 +0530

    added subtraction

--- Calculator.cpp
+++ Calculator.cpp
@@ -13,6 +13,10 @@ double divide(int a, int b)
 {
     return a / b;
 }
+double subtract(int a, int b)
+{
+    return a - b;
+}
 int main()
 {
     std::cout << "10 + 20 === " << add(10, 20)<< std::endl;
